049c273ed1216d405cd1c7389af62a44fb4f1a18
DiegoZC <dizuco25@gmail.com>
2024-04-16 20:36:39 +0200

    trigger tutorial

--- Practica6/Assets/Scripts/Tutorial.cs
+++ Practica6/Assets/Scripts/Tutorial.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject UiObject;
+    public GameObject trigger;
+    private void Start()
     {
-        
+        UiObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.GetComponent<PlayerMovement>() != null) 
+        {
+            UiObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        UiObject.SetActive(false);
     }
 }
