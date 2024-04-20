dd3b8c28fe7a2126a1698220ce9ce9da20523d57
Pavel.K144 <portex144@gmail.com>
2024-04-20 19:07:53 +0200

    MenuFunctional

---
+++ Practica6/Assets/Scripts/Mouse.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    Vector2 mousePosition;
+    private void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (mousePosition
+    }
+}
