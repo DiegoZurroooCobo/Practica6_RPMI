3c6518c1d6464c98c2983e53a04658cbef79d09c
Pavel.K144 <portex144@gmail.com>
2024-05-15 01:04:34 +0200

    LightsAndNewObjects

---
+++ Practica6/Assets/Scripts/SlimePadJump.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SlimePadJump : MonoBehaviour
+{
+    public float bounce;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Rigidbody2D>())
+        {
+            if (collision.GetContact(0).normal.y <= -0.9)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
+
+//if (collision.gameObject.gameObject.GetComponent<Rigidbody2D>())
+//{
+    //collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+//}
