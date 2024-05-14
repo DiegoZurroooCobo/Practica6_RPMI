ea0eb0788ae358a6cc140c46659d96a738107e71
AlonsoParaiso <alonpahe2@gmail.com>
2024-05-14 10:25:03 +0200

    arreglo fondo

---
+++ Practica6/Assets/Scripts/Character.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Jobs;
+using UnityEngine;
+
+public abstract class Character
+{
+    private float maxHealth;
+    public float health;
+    private string _name;
+    private Sprite _sprite;
+    protected float damage, mana;
+
+
+    public Character() { }
+    public Character(string name, float damage, Sprite sprite, float maxHealth, float mana) // para que todos los hijos del character tengan nombre danyo y un sprite 
+    {
+        this._name = name;
+        this.damage = damage;
+        _sprite = sprite;
+        this.maxHealth = maxHealth;
+        this.mana = mana;
+        health = maxHealth;
+    }
+
+
+    public Sprite GetSprite() // el metodo que aparecera en los hijos para los sprites  
+    {
+        return _sprite;
+    }
+    public float GetDamage()  // el metodo que aparecera en los hijos para el da�o 
+    {
+        return damage;
+    }
+
+    public string GetName() // el metodo que aparecera en los hijos para el nombre 
+
+    {
+        return _name;
+    }
+    public float GetMaxHealth()
+    {
+        return maxHealth;
+    }
+    
+    public virtual float Heal() //el metodo que aparecera en los hijos para la vida 
+    {
+        Debug.Log("Character se cura");
+        health = Mathf.Clamp(health, 0, maxHealth);  // lo clampeamos para que al curarse no sobrepase los 100 de vida 
+        return health;
+    }
+    public abstract float Attack(); // un metodo abstracto que no esta definido en la clase padre y que fuerzas a la clase hija 
+}
